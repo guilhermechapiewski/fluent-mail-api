04f79a4a8f3caf34f62ebeb5c2eda9fe01a5ac97
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2008-03-02 01:26:02 +0000

    Transport test.

---
+++ src-test/com/guilhermechapiewski/fluentmail/transport/PostalServiceTest.java
@@ -0,0 +1,13 @@
+package com.guilhermechapiewski.fluentmail.transport;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PostalServiceTest {
+
+	@Test
+	public void should_test_this_class_soon() {
+		fail("not implemented");
+	}
+}
